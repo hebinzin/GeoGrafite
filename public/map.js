3cd86caef077faf339c2b5cd151561050d0612aa
Heber <37860793+hebinzin@users.noreply.github.com>
2023-10-22 20:49:06 +0000

    Added Leaflet map as main page

---
+++ public/map.js
@@ -0,0 +1,46 @@
+let tileProvider = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
+// Alternative tile providers:
+// - https://openmaptiles.org/
+// - https://www.thunderforest.com/
+
+let layerOptions = {
+    minZoom: 4,
+    maxZoom: 16,
+    zoomDelta: 0.5,
+    attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+}
+
+const map = L.map('map')
+
+L.tileLayer(tileProvider, layerOptions).addTo(map);
+
+map.attributionControl.setPrefix(
+    '<a href="https://leafletjs.com/">Leaflet</a>'
+);
+
+map.locate({ setView: true, maxZoom: 16 });
+// TODO: if permission denied, get location from IP address (3rd party API)
+// - https://ipinfo.io/
+// - https://ip-api.com/
+// - https://iplocation.net/
+// - https://geolocation.com/
+// - https://ipgeolocation.io/
+// - https://geolocation-db.com/
+
+function onLocationFound(e) {
+    var radius = e.accuracy;
+    L.marker(e.latlng)
+        .addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point")
+        .openPopup();
+    L.circle(e.latlng, radius)
+        .addTo(map);
+}
+
+function onLocationError(e) {
+    alert(e.message);
+    // TODO: improve error design (e.g. show friendlier error message on map)
+}
+
+map.on('locationfound', onLocationFound);
+map.on('locationerror', onLocationError);
