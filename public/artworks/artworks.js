2efc33e9bde29a2f16ce15dba16e82ee18683cc3
Heber <37860793+hebinzin@users.noreply.github.com>
2023-11-27 03:13:40 +0000

    Added fake artworks

---
+++ public/artworks/artworks.js
@@ -0,0 +1,93 @@
+// Fake artworks data:
+export const artworks = [
+    {
+        id: 1,
+        title: 'Sou Nós',
+        artist: 'Aracê',
+        location: {
+            lat: -21.79318,
+            lng: -48.17924
+        },
+        image: './artworks/20230120_S2179318_E4817924.jpg'
+    },
+    {
+        id: 2,
+        title: 'Orig3m',
+        artist: 'Aracê',
+        location: {
+            lat: -21.79288,
+            lng: -48.17234
+        },
+        image: './artworks/20230408_S2179288_E4817234.jpg'
+    },
+    {
+        id: 3,
+        title: 'Sem título',
+        artist: 'Aracê',
+        location: {
+            lat: -21.79602,
+            lng: -48.18038
+        },
+        image: './artworks/20230707_S2179602-E4818038.jpg'
+    },
+    {
+        id: 4,
+        title: 'Músico',
+        artist: 'Desconhecido',
+        location: {
+            lat: -21.79661,
+            lng: -48.18128
+        },
+        image: './artworks/20230707_S2179661_E4818128.jpg'
+    },
+    {
+        id: 5,
+        title: 'Nossa Aracoara',
+        artist: 'Aracê',
+        location: {
+            lat: -21.78293,
+            lng: -48.20231
+        },
+        image: './artworks/20231120_S2178293_E4820231.jpg'
+    },
+    {
+        id: 6,
+        title: 'Páginas de Concreto',
+        artist: 'Aracê',
+        location: {
+            lat: -21.79262,
+            lng: -48.17998
+        },
+        image: './artworks/20231120_S2179262_E4817998.jpg'
+    },
+    {
+        id: 7,
+        title: 'Sou Sol',
+        artist: 'Aracê',
+        location: {
+            lat: -21.79446,
+            lng: -48.17865
+        },
+        image: './artworks/20231120_S2179446_E4817865.jpg'
+    },
+    {
+        id: 8,
+        title: 'Oni',
+        artist: 'Desconhecido',
+        location: {
+            lat: -21.79815,
+            lng: -48.18082
+        },
+        image: './artworks/20231126_S2179815_E4818082.jpg'
+    },
+    {
+        id: 9,
+        title: '"B"',
+        artist: 'Desconhecido',
+        location: {
+            lat: -21.79823,
+            lng: -48.18080
+        },
+        image: './artworks/20231126_S2179823_E4818080.jpg'
+    }
+];
--- public/map.js
+++ public/map.js
@@ -1,5 +1,24 @@
-let tileProvider = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
-// Alternative tile providers:
+import { artworks } from './artworks/artworks.js';
+
+// Artwork marker
+let pin = L.icon({
+    iconUrl: './img/pin.png',
+    iconAnchor: [14, 74],
+    popupAnchor: [10, -50]
+});
+
+// User location marker
+let me = L.icon({
+    iconUrl: './img/me.png',
+    iconAnchor: [25, 56.25],
+    popupAnchor: [0, -57]
+});
+
+let tiles = {
+    'osm': 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    'osmHOT': 'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
+}
+// Other tile providers:
 // - https://openmaptiles.org/
 // - https://www.thunderforest.com/
 
@@ -10,15 +29,15 @@ let layerOptions = {
     attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
 }
 
-const map = L.map('map')
+const map = L.map('map');
 
-L.tileLayer(tileProvider, layerOptions).addTo(map);
+L.tileLayer(tiles.osmHOT, layerOptions).addTo(map);
 
 map.attributionControl.setPrefix(
     '<a href="https://leafletjs.com/">Leaflet</a>'
 );
 
-map.locate({ setView: true, maxZoom: 16 });
+map.locate({ setView: true });
 // TODO: if permission denied, get location from IP address (3rd party API)
 // - https://ipinfo.io/
 // - https://ip-api.com/
@@ -27,11 +46,25 @@ map.locate({ setView: true, maxZoom: 16 });
 // - https://ipgeolocation.io/
 // - https://geolocation-db.com/
 
+
 function onLocationFound(e) {
-    var radius = e.accuracy;
-    L.marker(e.latlng)
+    let radius = e.accuracy;
+    let unit;
+    switch (true) {
+        case (radius < 1000):
+            unit = 'metros';
+            break;
+        case (radius < 10000):
+            unit = 'quilômetros';
+            distance = radius / 1000;
+            break;
+        default:
+            unit = 'mil quilômetros';
+            distance = radius / 1000000;
+    }
+    L.marker(e.latlng, { icon: me })
         .addTo(map)
-        .bindPopup("Você está a " + radius + " metros desse ponto.")
+        .bindPopup(`Você está a ${Math.round(radius)} ${unit} desse ponto.`)
         .openPopup();
     L.circle(e.latlng, radius)
         .addTo(map);
@@ -44,3 +77,10 @@ function onLocationError(e) {
 
 map.on('locationfound', onLocationFound);
 map.on('locationerror', onLocationError);
+
+for (let artwork of artworks) {
+    let popupContent = `<h2>${artwork.title}</h2><img src="${artwork.image}" width="200px">`;
+    L.marker(artwork.location, { icon: pin })
+        .addTo(map)
+        .bindPopup(popupContent);
+};
